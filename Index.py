99104ab8c64e8b74d578ddd62e453550e2be022a
THECODINGMASTER2014 <talwarmoksh@gmail.com>
2026-04-05 11:28:30 +0530

    a

---
+++ Index.py
@@ -0,0 +1,13 @@
+class Vehicle :
+    def __init__(self, name, max_speed, mileage):
+        self.name = name
+        self.max_speed = max_speed
+        self.mileage = mileage
+
+class Bus(Vehicle):
+    pass
+
+School_Bus = Bus("School Volvo", 180, 12)
+print("Vehicle Name :", School_Bus.name)
+print("Max Speed :", School_Bus.max_speed)
+print("Mileage :", School_Bus.mileage)
